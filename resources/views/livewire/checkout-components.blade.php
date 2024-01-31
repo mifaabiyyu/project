b3ed409e66fcec1e7564daa58886a13c56543cb0
jhonpaul77 <ninja.shakila@gmail.com>
2024-01-31 15:45:14 +0700

    11

--- resources/views/livewire/checkout-components.blade.php
+++ resources/views/livewire/checkout-components.blade.php
@@ -79,7 +79,9 @@ License: For each use you must have a valid license purchased only from above li
                                 <!--begin::Card body-->
                                 <div class="card-body pt-0">
                                     <div class=" text-center mt-5">
-                                        <h2>Checkout</h2>
+                                        <h4>Checkout</h4>
+                                        <h1>HectorAPP Services</h1>
+                                        <h3>Satu Aplikasi Untuk Semua Kebutuhan Bisnis Anda </h3>
                                     </div>
                                     <div class="table-responsive">
                                         <!--begin::Table-->
@@ -237,7 +239,7 @@ License: For each use you must have a valid license purchased only from above li
                                                         @endforeach
                                                         <!--begin::Grand total-->
                                                         <tr>
-                                                            <td colspan="2" class="fs-3 text-dark text-end">Grand Total</td>
+                                                            <td colspan="2" class="fs-3 text-dark text-end">Total</td>
                                                             <td class="text-dark fs-3 fw-boldest text-end">{{ "Rp " . number_format($data->total,0,'','.') }}</td>
                                                         </tr>
                                                         <!--end::Grand total-->
@@ -368,20 +370,43 @@ License: For each use you must have a valid license purchased only from above li
                                                     </div>
                                                 </div>
                                                 <!--end::Input group-->
-                                            
+                                               
+                                                        <!--begin::Notice-->
+														<div class="notice d-flex bg-light-primary rounded border-primary border border-dashed rounded-3 p-6">
+															<!--begin::Wrapper-->
+															<div class="d-flex flex-stack flex-grow-1">
+																<!--begin::Content-->
+																<div class="fw-semibold">
+																	<h4 class="justify-content-center text-gray-900 fw-bold">Total Pembelian :  {{ "Rp " . number_format($data->total,0,'','.') }} </h4>
+																	
+																</div>
+																<!--end::Content-->
+															</div>
+															<!--end::Wrapper-->
+														</div>
+														<!--end::Notice-->
+                                                        <br>
+                                        <button type="submit" id="kt_ecommerce_add_quote_submit" {{-- $validation==true?'':"disabled" --}} class="btn btn-primary w-100" data-kt-add-quotation="submit" >
+                                            <span class="svg-icon svg-icon-3">
+															<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+																<path d="M15.43 8.56949L10.744 15.1395C10.6422 15.282 10.5804 15.4492 10.5651 15.6236C10.5498 15.7981 10.5815 15.9734 10.657 16.1315L13.194 21.4425C13.2737 21.6097 13.3991 21.751 13.5557 21.8499C13.7123 21.9488 13.8938 22.0014 14.079 22.0015H14.117C14.3087 21.9941 14.4941 21.9307 14.6502 21.8191C14.8062 21.7075 14.9261 21.5526 14.995 21.3735L21.933 3.33649C22.0011 3.15918 22.0164 2.96594 21.977 2.78013C21.9376 2.59432 21.8452 2.4239 21.711 2.28949L15.43 8.56949Z" fill="currentColor" />
+																<path opacity="0.3" d="M20.664 2.06648L2.62602 9.00148C2.44768 9.07085 2.29348 9.19082 2.1824 9.34663C2.07131 9.50244 2.00818 9.68731 2.00074 9.87853C1.99331 10.0697 2.04189 10.259 2.14054 10.4229C2.23919 10.5869 2.38359 10.7185 2.55601 10.8015L7.86601 13.3365C8.02383 13.4126 8.19925 13.4448 8.37382 13.4297C8.54839 13.4145 8.71565 13.3526 8.85801 13.2505L15.43 8.56548L21.711 2.28448C21.5762 2.15096 21.4055 2.05932 21.2198 2.02064C21.034 1.98196 20.8409 1.99788 20.664 2.06648Z" fill="currentColor" />
+															</svg>
+														</span>
+                                            <span class="indicator-label" >Beli Sekarang</span>
+                                            <span class="indicator-progress">Please wait...
+                                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                        </button>
                                             
                                             </div>
                                         </div>
+                                        
                                     </form>
                                     <div class="d-flex justify-content-center mb-10">
                                         <!--begin::Button-->
                                         <!--end::Button-->
                                         <!--begin::Button-->
-                                        <button type="submit" id="kt_ecommerce_add_quote_submit" {{-- $validation==true?'':"disabled" --}} class="btn btn-success" data-kt-add-quotation="submit" >
-                                            <span class="indicator-label" >Checkout</span>
-                                            <span class="indicator-progress">Please wait...
-                                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                        </button>
+                                        
                                         <!--end::Button-->
                                     </div>
                                     <!--end::Product List-->
